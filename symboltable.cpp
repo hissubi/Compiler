e6eea441a2361ca2a0cd246df169eb91fcdf9ca3
ChoiJinWoo01 <chlwlsdn98@naver.com>
2021-12-10 23:08:02 +0900

    feat : unorderd symbol table

--- symboltable.cpp
+++ symboltable.cpp
@@ -4,12 +4,16 @@
 #include <fstream>
 #include "class.h"
 #include "extern.h"
+#include <stdio.h>
 
 using namespace std;
 
 void build_symbol_table(string input_file_name){
     ofstream symbol_file;
 	symbol_file.open(input_file_name + ".symbol");
+
+	int blockid = 0;
+
 	vector <Node*> check_tree;
     check_tree.push_back(root);
 
@@ -17,13 +21,34 @@ void build_symbol_table(string input_file_name){
     {
         Node* topnode = check_tree[0];
         check_tree.erase(check_tree.begin());
-        symbol_file << "data: " << topnode->data << "  child num: " << topnode->childn << endl;
-
-        symbol_file << "\t child: ";
+		if(topnode-> data == "block0"){	//block management : to seperate scope
+			blockid++;
+			symbol_file << endl;
+			symbol_file << "scope " << blockid << endl;
+			symbol_file << endl;
+		}
+		if(topnode->data == "decl0" && topnode->childn == 3){ //declare : put declared to symbol table
+			symbol_file << topnode->child[1]->child[0]->data << " ";	//name of the variable
+			symbol_file << topnode->child[0]->child[0]->data << " ";	//type
+			if(topnode -> child[0]->child[0]->data == "int"){
+				symbol_file << "4 ";
+			}
+			else{
+				symbol_file << "1 ";
+			}
+			symbol_file << endl;
+		}
+		if(topnode->data == "prog0"){	//add function to symbol table
+			symbol_file << topnode->child[0]->child[0]->data << " ";
+			symbol_file << "func";
+			symbol_file << endl;
+		}
+        cout << "data : " << topnode->data << "  child num: " << topnode->childn << endl;
+        cout << "\t child : ";
         for(int i = 0; i < topnode->childn; i++)
         {
             Node* tmpnode = topnode->child[i];
-            symbol_file << tmpnode->data << " ";
+            cout << tmpnode->data << " ";
             check_tree.insert(check_tree.begin()+i, tmpnode);
         }
         cout << endl;
