48f1afb1782b8272ebfda0c5ecfd445b22a6e497
jonguk6108 <jonguk6108@gmail.com>
2021-12-11 20:33:42 +0900

    feat : symbol addr

--- symboltable.cpp
+++ symboltable.cpp
@@ -18,9 +18,9 @@ void build_symbol_table(string input_file_name){
 	vector <vector <vector <string>>> symbol_table_scopes;
 	vector <Node*> check_tree;
 
-  root->resistor_num = 0;
-  root->label_num = 0;
-  root->scope_num = 0;
+  root->resistor = 0;
+  root->label = 0;
+  root->scope = 0;
   check_tree.push_back(root);
 
 	while(check_tree.size() !=0) {
@@ -29,12 +29,15 @@ void build_symbol_table(string input_file_name){
     Node* topnode = check_tree[0];
     check_tree.erase(check_tree.begin());
 
-		if(topnode-> data == "{") {	//block management : to seperate scope
+		if(topnode-> data == "block0") {	//block management : to seperate scope
 			symbol_table_scopes.push_back(symbol_table);
 			symbol_table.clear();
 			blockid++;
+      
+      topnode->scope = blockid;
 		}
 
+    cout << "1\n";
 		if(topnode->data == "decl0" && topnode->childn == 3){ //declare : put declared to symbol table
 			buf.push_back(topnode->child[1]->child[0]->data);	//name
 			buf.push_back(topnode->child[0]->child[0]->data);	//type
@@ -44,18 +47,41 @@ void build_symbol_table(string input_file_name){
 			else if(topnode->child[0]->child[0]->data == "char"){
 				buf.push_back("1");
 			}
+
+      int addr = 0;
+      if(symbol_table.size() == 0) {
+        int c_scope = topnode->scope;
+        Node* c_node = topnode;
+
+        while(c_node->parent != NULL) {
+          if(c_node->scope != c_scope && symbol_table_scopes[c_node->scope].size() != 0)
+            break;
+          c_node = c_node->parent;
+        }
+        c_scope = c_node->scope;
+
+	      vector <vector <string>> c_symbol_table = symbol_table_scopes[c_scope];
+        addr = stoi(c_symbol_table[c_symbol_table.size() - 1][3]) + stoi(c_symbol_table[c_symbol_table.size() - 1][2]);
+      }
+      else
+        addr = stoi(symbol_table[symbol_table.size() - 1][3]) + stoi(symbol_table[symbol_table.size() - 1][2]);
+
+      buf.push_back(to_string(addr));
 		}
 		if(topnode->data == "prog0"){	//add function to symbol table
-			buf.push_back(topnode->child[0]->child[0]->data);
-			buf.push_back("func");
+			buf.push_back(topnode->child[0]->child[0]->data); //name
+			buf.push_back("func"); //type
+      buf.push_back("0"); //size
+      buf.push_back("1000");
 		}
 
     //cout << "data : " << topnode->data << "  child num: " << topnode->childn << endl;
     //cout << "\t child : ";
     for(int i = 0; i < topnode->childn; i++) {
       Node* tmpnode = topnode->child[i];
-      // set parent node
-      tmpnode->parent = topnode;
+      tmpnode->scope = topnode->scope;
+      tmpnode->resistor = topnode->resistor;
+      tmpnode->label = topnode->label;
 
       //cout << tmpnode->data << " ";
       check_tree.insert(check_tree.begin()+i, tmpnode);
